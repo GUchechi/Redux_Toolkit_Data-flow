ea96b1a68391b48fa486a93887be21b247588f86
GUchechi <godswilluchendu7@gmail.com>
2022-11-30 18:10:42 +0100

    blog-app

--- src/features/posts/PostLists.js
+++ src/features/posts/PostLists.js
@@ -2,21 +2,25 @@ import React from "react";
 import { useSelector } from "react-redux";
 import PostAuthor from "./PostAuthor";
 import { selectAllPosts } from "./postSlice";
+import ReactionButtons from "../posts/ReactionButtons";
 import TimeAgo from "./TimeAgo";
 
 const PostLists = () => {
-  const posts = useSelector (selectAllPosts);
+  const posts = useSelector(selectAllPosts);
 
-  const orderedPosts = posts.slice().sort((a, b) => b.date.localeCompare(a.date))
+  const orderedPosts = posts
+    .slice()
+    .sort((a, b) => b.date.localeCompare(a.date));
 
   const renderedPosts = orderedPosts.map((post) => (
     <article key={post.id}>
       <h3>{post.title}</h3>
       <p>{post.content.substring(0, 100)}</p>
       <p className="postCredit">
-        <PostAuthor userId={post.userId}/>
-        <TimeAgo timestamp={post.date}/>
+        <PostAuthor userId={post.userId} />
+        <TimeAgo timestamp={post.date} />
       </p>
+      <ReactionButtons post={post} />
     </article>
   ));
   return (
