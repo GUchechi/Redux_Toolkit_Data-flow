fee6bbbe8c848e24f66ffe23ad63f9ae2c659338
GUchechi <godswilluchendu7@gmail.com>
2022-12-01 02:06:38 +0100

    blog-app

--- src/features/users/usersSlice.js
+++ src/features/users/usersSlice.js
@@ -1,18 +1,28 @@
-import { createSlice } from "@reduxjs/toolkit";
 
-const initialState = [
 
-    { id:"0", name: 'Dude Leboski'},
-    { id:"1", name: 'Neil Young'},
-    { id:"2", name: 'Godswill Uchechi'}
-]
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import axios from "axios";
 
-const userSlice = createSlice({
-    name: "users",
+const USERS_URL = 'https://jsonplaceholder.typicode.com/users';
+
+const initialState = []
+
+export const fetchUsers = createAsyncThunk('users/fetchUsers', async () => {
+    const response = await axios.get(USERS_URL);
+    return response.data
+})
+
+const usersSlice = createSlice({
+    name: 'users',
     initialState,
-    reducers: {}
+    reducers: {},
+    extraReducers(builder) {
+        builder.addCase(fetchUsers.fulfilled, (state, action) => {
+            return action.payload;
+        })
+    }
 })
 
 export const selectAllUsers = (state) => state.users;
 
-export default userSlice.reducer;
+export default usersSlice.reducer
